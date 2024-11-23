8ca08ea26ce58fd224259ac7a44ae3c62c21bc99
Dipesh Arwat <dipesharwat80@gmail.com>
2024-11-23 09:34:04 +0530

    change

--- client/src/api/AuthRequests.js
+++ client/src/api/AuthRequests.js
@@ -3,6 +3,14 @@ import axios from 'axios'
 
 const API = axios.create({ baseURL: 'https://mern-socialmedia-master-backend.onrender.com' });
 
+API.interceptors.request.use((req) => {
+    const profile = localStorage.getItem('profile');
+    if (profile) {
+        req.headers.Authorization = `Bearer ${JSON.parse(profile).token}`;
+    }
+    return req;
+});
+
 export const logIn= (formData)=> API.post('/auth/login',formData);
 
 export const signUp = (formData) => API.post('/auth/register', formData);
