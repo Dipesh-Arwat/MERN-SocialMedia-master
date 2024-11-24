3bce42eb64913c76e394f885e244a34e38ce5b3f
Dipesh Arwat <dipesharwat80@gmail.com>
2024-11-24 11:41:40 +0530

    changes

--- server/upload.js
+++ server/upload.js
@@ -4,7 +4,7 @@ import path from "path";
 // Define storage engine for multer
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "public/images/"); 
+    cb(null, "uploads/"); 
   },
   filename: (req, file, cb) => {
     cb(
