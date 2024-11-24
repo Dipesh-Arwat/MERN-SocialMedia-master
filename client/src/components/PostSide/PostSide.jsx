bcb62a2996c6e70a43a68dcce4df7aefbe2fe221
Dipesh Arwat <dipesharwat80@gmail.com>
2024-11-24 19:05:53 +0530

    changes

--- client/src/components/PostSide/PostSide.jsx
+++ client/src/components/PostSide/PostSide.jsx
@@ -2,32 +2,12 @@ import React, { useEffect, useState } from "react";
 import Posts from "../Posts/Posts";
 import PostShare from "../PostShare/PostShare";
 import "./PostSide.css";
-import axios from "axios";
 
 const PostSide = () => {
-
-  const [stories, setStories] = useState([]);
-
-  useEffect(() => {
-    const fetchStories = async () => {
-      const { data } = await axios.get("https://mern-socialmedia-master-backend.onrender.com/stories");
-      setStories(data);
-    };
-
-    fetchStories();
-  }, []);
 
   return (
     <div className="PostSide">
       <PostShare/>
-      <div className="Stories">
-        {stories.map((story) => (
-          <div key={story._id}>
-            <img src={story.image} alt="story" />
-            <span>{story.userId.username}</span>
-          </div>
-        ))}
-      </div>
       <Posts/>
     </div>
   );
